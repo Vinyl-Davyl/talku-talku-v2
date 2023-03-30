9cbe45301752f5f7a569d174d0c3b06019cc6fde
Vinyl-Davyl <okononfuadavid@gmail.com>
2023-03-30 22:38:11 +0100

    Update

--- public/src/utils/APIRoutes.js
+++ public/src/utils/APIRoutes.js
@@ -1,4 +1,7 @@
-export const host = "http://localhost:5000";
+export const host = process.env.NODE_ENV === "production"
+  ? "https://talku-talku-v2.vercel.app"
+  : "http://localhost:5000";
+
 export const loginRoute = `${host}/api/auth/login`;
 export const registerRoute = `${host}/api/auth/register`;
 export const logoutRoute = `${host}/api/auth/logout`;
